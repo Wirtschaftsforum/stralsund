6187c34f224ffe139dffff6808e3b14d38d2dd65
Wirtschaftsforum <wirtschaftsforum@makerport.de>
2026-08-27 18:24:05 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -10,5 +10,5 @@
 // dieses Keys. Trage hier niemals den "service_role" Key ein!
 // ============================================================
 
-window.SUPABASE_URL = "https://ndmgkkinqpyipjkkohqr.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_lZjxWUtByrAidBOioikPKg_iG0UjQIA";
+window.SUPABASE_URL = "https://bajftvuobpiqxsowqrde.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_V8ljTd-UEcScW5T3k_OFQg_x69WRQA0";
